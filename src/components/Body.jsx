85abf4d250826a5ce948f0879212f8971afd8f57
Kalu David <kaludavidinyang@gmail.com>
2024-09-24 13:39:31 +0100

    last commit I guess

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -9,7 +9,7 @@ export function Body({ children, handleSubmit }) {
     return (
         <>
             <main className="w-full h-full overflow-x-hidden flex items-center justify-center font-Dm bg-[#FFFFFF] ">
-                <div className='w-[404px] flex items-center justify-center flex-col my-[93px] gap-5'>
+                <div className='w-[404px] flex items-center justify-center flex-col my-[93px] gap-5 max-[430px]:mx-[5%]'>
                     <img src={logo} alt="galileo Logo" />
                     <div className='bg-[#FFFFFF] border border-[#D9D8DD] drop-shadow-md rounded-2xl w-full pt-[25px]  pb-[50px] '>
                         <GoogleAndTwitter navigate={navigate} />
